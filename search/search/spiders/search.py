815dc1262296ff962f6162a02cdad90f12b87ea9
cesar <csgalindos@hotmail.com>
2020-12-13 20:47:25 -0500

    [updated] review order refactoring some calls

--- search/search/spiders/search.py
+++ search/search/spiders/search.py
@@ -21,13 +21,35 @@ class Item(scrapy.Spider):
     }
 
     def parse(self, response):
+        # date information
         today = datetime.date.today().strftime('%d-%m-%Y')
+
+        # Language Tag
+        language_tag = response.xpath('/html/@lang').get()
+        if language_tag:
+            booleanlanguage_tag = True
+        else:
+            booleanlanguage_tag = False
+
+        # Title
+        # get title tag
         titlepage = response.xpath('//title/text()').get()
+        # get number of titles tag in page
+        numbertitlepage = len(response.xpath('//title/text()').getall())
+
+        # tag Title duplicated
+        if numbertitlepage == 1:
+            title_duplicated = False
+        else:
+            title_duplicated = True
+
+        # check if exist some information in title tag
         if titlepage == None:
             sizetitlepage = False
         else:
             sizetitlepage = len(titlepage)
 
+        # check dimension of text in title tag
         if sizetitlepage == 0:
             rigthdimentiontitlepage = 'untitle'
         elif sizetitlepage <= 60 and sizetitlepage >= 50:
@@ -38,8 +60,8 @@ class Item(scrapy.Spider):
             rigthdimentiontitlepage = False
         else:
             rigthdimentiontitlepage = False
-        numbertitlepage = len(response.xpath('//title/text()').getall())
 
+        # check dimension of text in title tag
         if numbertitlepage >= 0:
             booleannumbertitlepage = False
         elif numbertitlepage < 2:
@@ -47,117 +69,97 @@ class Item(scrapy.Spider):
         else:
             booleannumbertitlepage = True
 
+        # Meta Description
+        # meta description information
         meta_description = response.xpath(
             '//meta[@name="description"]/@content').get()
+
+        # check number of meta description tags
+        numbermeta_description = len(response.xpath(
+            '//meta[@name="description"]/@content').getall())
+
+        # dimension of meta description tag
         sizemetadescription = len(meta_description)
+
+        # check of dimension about meta description tag
         if sizemetadescription == 0:
             rigthdimentionmetadescription = 'undescription'
         elif sizemetadescription <= 155:
             rigthdimentionmetadescription = True
         else:
             rigthdimentionmetadescription = False
-        numbermeta_description = len(response.xpath(
-            '//meta[@name="description"]/@content').getall())
 
+        # Meta Keyword
+        # meta Keyword information
         meta_keywords = response.xpath(
             '//meta[contains(@name,"keywords")]').getall()
+
+        # check number of meta keyword tags
         numbermeta_keywords = len(response.xpath(
             '//meta[contains(@name,"keywords")]').getall())
 
+        # H tags
+        # get information of H1 tag
         htitle = response.xpath('//h1/text()').get()
+        # number of H1 tag have
         numberhtitle = len(response.xpath('//h1/text()').getall())
-        if numberhtitle <= 1:
-            boolean_numberhtitle = False
-        else:
+        # Tags H1 y H2 Counter
+        number_tags_h1 = len(response.xpath('//h1/text()').getall())
+        number_tags_h2 = len(response.xpath('//h2/text()').getall())
+
+        # review if exist some
+        if numberhtitle == 1:
             boolean_numberhtitle = True
+        else:
+            boolean_numberhtitle = False
 
+        # img
+        # get img without alt atribute
         imgwithoutalt = response.xpath('//img[not(@alt)]').getall()
-
+        # total elements the img of the page
+        totalimg = len(response.xpath('//img').getall())
+        # number of img without alt atribute
+        numberimgwithoutalt = len(response.xpath('//img[not(@alt)]').getall())
+        # img with attribute alt empty
+        imgwithaltempty = response.xpath('//img[@alt=""]/@src').getall()
+        # return tha number of img empty
+        numberimgwithaltempty = len(response.xpath('//img[@alt=""]').getall())
+        # show the percentage of img with something with alt empty
+        percentajeemptyaltinimg = int((numberimgwithaltempty/totalimg)*100)
+        # check number of img rith
         if len(imgwithoutalt) >= 0:
             booleanimgwithoutalt = False
         else:
             booleanimgwithoutalt = True
 
-        numberimgwithoutalt = len(response.xpath('//img[not(@alt)]').getall())
-
-        imgwithaltempty = response.xpath('//img[@alt=""]/@src').getall()
-        numberimgwithaltempty = len(response.xpath('//img[@alt=""]').getall())
-
-        totalimg = len(response.xpath('//img').getall())
-        percentajeemptyaltinimg = int((numberimgwithaltempty/totalimg)*100)
-
-        meta_google = response.xpath(
-            '//meta[contains(@property,"og")]').getall()
-        meta_facebook = response.xpath(
-            '//meta[contains(@property,"fb")]').getall()
-        meta_twitter = response.xpath(
-            '//meta[contains(@name,"twitter")]').getall()
-
-        if (meta_twitter and meta_facebook and meta_google):
-            boolean_meta_sm = True
-        else:
-            boolean_meta_sm = False
-
-        # New Features
-
-        # Tag Title duplicated
-        titles_page = response.xpath('//title/text()').getall()
-        if len(titles_page) == 1:
-            title_duplicated = False
-        else:
-            title_duplicated = True
-
-        # Old tags
+        # old tags
+        # check if the page use some old tags
         old_tags = []
-        list_old_tags = ['applet', 'acronym', 'bgsound', 'dir', 'frame', 'frameset', 'noframes',
-                         'hgroup', 'isindex', 'listing', 'xmp', 'noembed', 'strike', 'basefont',
-                         'big', 'blink', 'center', 'font', 'marquee', 'multicol', 'nobr', 'spacer'
-                         'tt', 'menu']
+        list_old_tags = ['applet', 'acronym', 'bgsound', 'dir', 'frame', 'frameset', 'noframes', 'hgroup', 'isindex', 'listing', 'xmp',
+                         'noembed', 'strike', 'basefont', 'big', 'blink', 'center', 'font', 'marquee', 'multicol', 'nobr', 'spacer''tt', 'menu']
         for tag in list_old_tags:
             found_tags = response.xpath(f'//{tag}').getall()
             if len(found_tags) > 0:
                 old_tags.append(found_tags)
 
+        # retur a boolena information if use someone old tags
         if old_tags:
             boolean_old_tags = False
         else:
             boolean_old_tags = True
 
-        # Tag Button without Arial-labels
+        # Aria - label
+        # tag Button without Arial-labels
         buttons = response.xpath('//button').getall()
         buttons_wo_aria = response.xpath('//button[not(@aria-label)]').getall()
         inputs = response.xpath('//input').getall()
         inputs_wo_aria = response.xpath('//input[not(@aria-label)]').getall()
 
-        # count_buttons = response.xpath('//button/@aria-label').getall()
-        # for i in count_buttons:
-        #     if len(i) <= 6:
-        #         dimension_buttons = True,
-        #     else:
-        #         dimension_buttons = False
-
-        # count_inputs = response.xpath('//input/@aria-label').getall()
-        # for i in count_inputs:
-        #     if len(i) <= 6:
-        #         dimension_inputs = True,
-        #     else:
-        #         dimension_inputs = False
-
+        # review about if buttons and inputs have the aria label
         if ((buttons_wo_aria == buttons) and (inputs_wo_aria == inputs)):
             boolean_aria = True
         else:
             boolean_aria = False
-
-            # Tags H1 y H2 Counter
-        number_tags_h1 = len(response.xpath('//h1/text()').getall())
-        number_tags_h2 = len(response.xpath('//h2/text()').getall())
-
-        # Language Tag
-        language_tag = response.xpath('/html/@lang').get()
-        if language_tag:
-            booleanlanguage_tag = True
-        else:
-            booleanlanguage_tag = False
 
         # Semanthic Structure
         # Header is True?
@@ -184,55 +186,40 @@ class Item(scrapy.Spider):
         if len(footer_found) == 1:
             footer = True
 
+        # validate if some tags like header, main, footer or section exits in the web page
         if ((footer == True) and (header == True) and (main == True) and (section == True)):
             boolean_structure_semantic = True
         else:
             boolean_structure_semantic = False
+
+        # social meta tags
+        # review the information of set in faceboo, facebook and twitter
+        meta_google = response.xpath(
+            '//meta[contains(@property,"og")]').getall()
+        meta_facebook = response.xpath(
+            '//meta[contains(@property,"fb")]').getall()
+        meta_twitter = response.xpath(
+            '//meta[contains(@name,"twitter")]').getall()
+
+        # review if exist information of social media tags
+        if (meta_twitter and meta_facebook and meta_google):
+            boolean_meta_sm = True
+        else:
+            boolean_meta_sm = False
 
         yield {
             'date': today,
             'titlepage': titlepage,
             'metadescription': meta_description,
 
-            'titleAT': "Alternativas Textuales",
-            'descriptionAT': "Todos los elementos no visuales de la pagina deben incluir la etiqueta alt para facilitar la lectura por el robot de Google.",
             'booleanAT': booleanimgwithoutalt,
-            'endpointAT': "/textualalternatives",
-
-            'titleTP': "Titulo de Pagina",
-            'descriptionTP': "Unicamente debe exister una etiqueta title dentro de la estructura de la pagina y el contendo de esta debe ser descriptivo en relación al contenido de la pagina.",
             'booleanTP': booleannumbertitlepage,
-            'endpointTP': "/titlepage",
-
-            'titleDI': "Declaración de Idioma",
-            'descriptionDI': "Se debe especificar el idioma de la pagina con la etiqueta: <html lang=eng>.",
             'booleanDI': booleanlanguage_tag,
-            'endpointDI': "ee",
-
-            'titleES': "Estructura Semantica",
-            'descriptionES': "La pagina deberia utilizar un mínimo de etiquetas semánticas las cuales son: header, section, footer, main.",
             'booleanES': boolean_structure_semantic,
-            'endpointES': "el endpoint a consular para este feature",
-
-            'titleEA': "Etiqueta Arial-label",
-            'descriptionEA': "Las etiquetas de button e input deberian tener el atributo aria-label y el contenido de este debe ser superior a 6 caracteres.",
             'booleanEA': boolean_aria,
-            'endpointEA': "el endpoint a consular para este feature",
-
-            'titleJT': "Jerarquias Textuales",
-            'descriptionJT': "La etiquetas de titulos deben seguir un jerarquia de acuerdo a su orden, solo deberia usarse una vez la etiqueta <h1> por ejemplo.",
             'booleanJT': boolean_numberhtitle,
-            'endpointJT': "/textualhierarchies",
-
-            'titleED': "Etiquetas en Desuso",
-            'descriptionED': "Con el paso de las versiones de html se han dejado de usar ciertas etiquetas, se recomienda su sustitución.",
             'booleanED': boolean_old_tags,
-            'endpointED': "/disusedlabels",
-
-            'titleMI': "Meta Información",
-            'descriptionMI': "Se recomienda el uso de las etiquetas de meta información para asegurar que el robot de google entienda nuestra pagina. Así como para la correcta representación en redes sociales.",
             'booleanMI':  boolean_meta_sm,
-            'endpointMI': "/metadescription",
 
             'imgwithoutalt': imgwithoutalt,
             'qtyimgwithoutalt': numberimgwithoutalt,
@@ -267,6 +254,5 @@ class Item(scrapy.Spider):
             'number_tags_h2': number_tags_h2,
             'language_tag': language_tag,
             'header': header,
-            # 'body': body,
             'footer': footer
         }
